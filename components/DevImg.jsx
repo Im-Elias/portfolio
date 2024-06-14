cbc741c044d1a61653a0e04c2d44e13af20abe49
Josue Gallardo <dotelias02@gmail.com>
2024-06-14 14:33:13 -0400

    added Favicon

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const DevImg = ({ containerStyles, imgSrc }) => {
    return (
       <div className={`${containerStyles}`}>
-         <Image src={imgSrc} fill priority alt="Yo" />
+         <Image src={imgSrc} fill priority sizes="auto" alt="Yo" />
       </div>
    );
 };
