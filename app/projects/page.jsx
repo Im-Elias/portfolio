f0da1e5f281d6c2c5e95ab1f46cd6b9cedb65f94
Josue Gallardo <dotelias02@gmail.com>
2024-06-17 12:22:06 -0400

    front end listo, falta envio de mail

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,117 @@
+"use client";
+import React, { useState } from "react";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/ProjectCard";
+
+const projectsData = [
+  {
+    image: "/work/1.png",
+    category: "next js",
+    name: "Lumina Website",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptas.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "react js",
+    name: "Nexa Website",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptas.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "fullstack",
+    name: "Serenity Website",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptas.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "next js",
+    name: "Soltice Website",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptas.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "react js",
+    name: "Stark Website",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptas.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "fullstack",
+    name: "Swift Website",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptas.",
+    link: "/",
+    github: "/",
+  },
+];
+
+//remove category duplicates
+const uniqueCategories = [
+  "todos",
+  ...new Set(projectsData.map((item) => item.category)),
+];
+
 const Projects = () => {
-   return <div>Projects Page</div>;
+  const [categories, setCategories] = useState(uniqueCategories);
+  const [category, setCategory] = useState("todos");
+
+  const filteredProjects = projectsData.filter((project) => {
+    //si al categoria es todos, retornar todos los proyectos, si no, filtrar por categoria
+    return category === "todos" ? project : project.category === category;
+  });
+
+  console.log(filteredProjects);
+
+  return (
+    <section className=" min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          Mis Proyectos
+        </h2>
+        {/* tabs */}
+        <Tabs defaultValue={category} className="mb-24 xl:mb-32">
+          <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] md-12 mx-auto md:border dark:border-none">
+            {categories.map((category, index) => {
+              return (
+                <TabsTrigger
+                  onClick={() => setCategory(category)}
+                  value={category}
+                  key={index}
+                  className="capitalize w-[162px] md:w-auto">
+                  {category}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+          {/* tabs content */}
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project}></ProjectCard>
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
---
+++ components/Form.jsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { User, MailIcon, ArrowRightIcon, MessageSquare } from "lucide-react";
+import { useState } from "react";
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendMail = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("api/sendEmail", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ name, email, message }),
+    });
+    console.log(await response.json());
+  };
+
+  return (
+    <form onSubmit={sendMail} className="flex flex-col gap-y-4">
+      {/* input */}
+      <div className="relative flex items-center">
+        <Input
+          type="name"
+          id="name"
+          required
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="Nombre"
+        />
+        <User className="absolute right-6" size={20} />
+      </div>
+      {/* input */}
+      <div className="relative flex items-center">
+        <Input
+          type="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Email"
+        />
+        <MailIcon className="absolute right-6" size={20} />
+      </div>
+      {/* textarea */}
+      <div className="relative flex items-center">
+        <Textarea
+          name="message"
+          id="message"
+          required
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          placeholder="Escribe tu mensaje aquí."
+        />
+        <MessageSquare className="absolute top-4 right-6" size={20} />
+      </div>
+      <Button
+        type="submit"
+        className="flex items-center  gap-x-1 max-w-[166px]">
+        Hablemos
+        <ArrowRightIcon size={20} />
+      </Button>
+    </form>
+  );
+};
+
+export default Form;
